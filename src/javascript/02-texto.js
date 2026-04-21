3faaf8c28d50e05072083ca7b187cdf29edd2a38
Shadow-Rider-12 <aaronexekien@gmail.com>
2026-04-20 23:25:27 -0400

    create a alert and texto

---
+++ src/javascript/02-texto.js
@@ -0,0 +1,15 @@
+
+//definimos variables "CON VAR"
+var nombre= "Aaron";
+var apellido= "Alvares";
+var edad= 20;
+
+//Mostrar texto en console.log
+console.log( nombre + " "+ apellido, +" " + edad );
+console.log( nombre, apellido, edad );
+
+var nom_ape= nombre + " " + apellido;
+
+console.log(nom_ape," "+ edad );
+
+
---
+++ src/javascript/05-alert.js
@@ -0,0 +1,12 @@
+
+
+//Definir varaibles
+var pais = "Chile";
+var continente = "South-America";
+
+alert(pais);
+alert(continente);
+
+let a = 12;
+let b = 73;
+alert( a+b );
