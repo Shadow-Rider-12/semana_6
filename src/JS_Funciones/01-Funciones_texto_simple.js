e418da3f55ab0d9c92b27bd3d5c7e3ad6a0195f9
Shadow-Rider-12 <aaronexekien@gmail.com>
2026-04-28 21:51:04 -0400

    continue with for and create funciones_texto_simple.js

---
+++ src/JS_Funciones/01-Funciones_texto_simple.js
@@ -0,0 +1,20 @@
+//Definimos una variable
+let nombre = "Aaron";
+//Creamos la primera funcion 
+function saludar(){
+    console.log ("Hola Profe " + nombre + "!");
+    console.log ("Estamos en clase de Desarrollo Web");
+}
+saludar();
+
+console.log("--------------------------")
+console.log("--------------------------")
+
+function saludar2(apellido){
+    console.log("Hola ", + apellido + "!");
+}
+console.log("Alvares");
+
+console.log("--------------------------")
+console.log("--------------------------")
+
--- src/javascript/08-for.js
+++ src/javascript/08-for.js
@@ -6,16 +6,19 @@ for (var i = 1; i <= numero; i++){
     console.log("Numero: ", i);
 }
 console.log("--------------------");
+console.log("--------------------");
 
 for (var i = 10; i >= 1; i--){
     console.log("Numero: ", i);
 }
+console.log("--------------------");
 console.log("--------------------");
 
 //Buscamos el numero par, pero notar que estamos sumando 2
 for(var i=2; i <= numero; i+= 2){
     console.log("Par: ", i);
 }
+console.log("------------------------------")
 console.log("------------------------------")
 
 //Identificamos los numeros par e impar
@@ -27,3 +30,65 @@ for(var i = 1; i <= numero; i++){
         console.log(i + " Es Impar");
     }
 }
+
+console.log("------------------------------")
+console.log("------------------------------")
+var frutas = ["manzana", "pera", "uva", "naranja"]
+
+for (var i = 0; i < frutas.length; i++){
+    console.log("Fruta: ", frutas);
+}
+
+console.log("------------------------------")
+console.log("------------------------------")
+
+//forEach
+frutas.forEach(function (fruta){
+    console.log("Fruta con ForEach: ", fruta);
+}
+);
+
+console.log("------------------------------")
+console.log("------------------------------")
+
+//Version optimizada para listas mas pequeñas 
+for (var fruta of frutas){
+    console.log("Fruta: ", fruta);
+}
+console.log("------------------------------")
+console.log("------------------------------")
+
+var ucm = ["IronMan", "Hulk", "Thor", "Gru"];
+for (var i of ucm){
+    console.log("Personajes: ", i);
+}
+
+console.log("------------------------------")
+console.log("------------------------------")
+
+var ucmMayusculas = ucm.map(function (ucm){
+    return ucm.toUpperCase();
+});
+console.log(ucmMayusculas);
+
+console.log("------------------------------")
+console.log("------------------------------")
+
+var ucmMinuscula = ucm.map(function (ucm){
+    return ucm.toLowerCase();
+});
+console.log(ucmMinuscula);
+
+console.log("------------------------------")
+console.log("------------------------------")
+
+//Definir el Array
+var bandas = ["Adema", "AC/DC", "Angra", "As lie day"];
+//Filtrar bandas que tenga 5 caracteres
+var bandasCortas = bandas.filter(function (filtro){
+    return filtro.length <= 5;
+})
+console.log(bandasCortas);
+
+console.log("------------------------------")
+console.log("------------------------------")
